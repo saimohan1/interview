17987892044db9c331464a4aa4ba92b98010f51e
L Saimohan Rao <saimohanrao1@gmail.com>
2023-10-21 23:02:55 +0530

    added one more container_of variant
    Signed-off-by: L Saimohan Rao <saimohanrao1@gmail.com>

--- c/container_of.c
+++ c/container_of.c
@@ -13,10 +13,13 @@
 //		(type*)(__mptr - offsetof(type, member));})
 
 /* 3rd method */
-#define container_of(ptr, type, member)	({\
-		const typeof(((type*)0)->member) *__mptr = (ptr);	\
-		(type*)((char*)__mptr - offsetof(type, member));})
+//#define container_of(ptr, type, member)	({\
+//		const typeof(((type*)0)->member) *__mptr = (ptr);	\
+//		(type*)((char*)__mptr - offsetof(type, member));})
 
+
+#define container_of(ptr, type, member)	({	\
+		(type*)((void*)(ptr) - offsetof(type, member));})
 
 struct temp {
 	int a;
